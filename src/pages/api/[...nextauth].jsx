533905a99df9d59a3ede57e418631feb354ffe03
cesardmn <cesardmn@gmail.com>
2023-05-15 08:38:27 -0300

    add callback in nextath settings

--- src/pages/api/[...nextauth].jsx
+++ src/pages/api/[...nextauth].jsx
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions = {
   providers: [
@@ -8,6 +8,19 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
 }
 
 export default NextAuth(authOptions)

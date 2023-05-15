8b5179e5d444076e4e7a0ac50e677cb03996c80c
cesardmn <cesardmn@gmail.com>
2023-05-15 19:41:31 -0300

    readme update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,35 @@
 import Display from '@src/components/Display'
 import Profile from '@src/components/Profile'
 import Repos from '@src/components/Repos'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="app">
-      <section className="profile">
-        <Profile />
-      </section>
-      <section className="repos">
-        <Repos />
-      </section>
-      <section className="display">
-        <Display />
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>GitShow</title>
+        <meta
+          name="description"
+          content="A simpler way to view your GitHub repositories."
+        />
+        <link
+          rel="icon"
+          class="js-site-favicon"
+          type="image/svg+xml"
+          href="favicon.svg"
+        ></link>
+      </Head>
+      <div className="app">
+        <section className="profile">
+          <Profile />
+        </section>
+        <section className="repos">
+          <Repos />
+        </section>
+        <section className="display">
+          <Display />
+        </section>
+      </div>
+    </>
   )
 }
